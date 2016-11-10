9b232092cd097d284669723fefbc03a58fa617b9
John Clemson <john.clemson@ionchannel.io>
2016-11-10 08:47:39 -0800

    Adding jacoco plugin, adding new methods and tests to up code coverage

--- src/test/java/io/ionchannel/example/AppTest.java
+++ src/test/java/io/ionchannel/example/AppTest.java
@@ -28,6 +28,66 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
+
+    public void testOne(){
+        App myApp = new App();
+        assertTrue(myApp.return_one() == 1);
+    }
+
+
+    public void testTwo(){
+        App myApp = new App();
+        assertTrue(myApp.return_two() == 2);
+    }
+
+
+    public void testThree(){
+        App myApp = new App();
+        assertTrue(myApp.return_three() == 3);
+    }
+
+
+    public void testFour(){
+        App myApp = new App();
+        assertTrue(myApp.return_four() == 4);
+    }
+
+
+    public void testFive(){
+        App myApp = new App();
+        assertTrue(myApp.return_five() == 5);
+    }
+
+
+    public void testSix(){
+        App myApp = new App();
+        assertTrue(myApp.return_six() == 6);
+    }
+
+
+    public void testSeven(){
+        App myApp = new App();
+        assertTrue(myApp.return_seven() == 7);
+    }
+
+
+    public void testEight(){
+        App myApp = new App();
+        assertTrue(myApp.return_eight() == 8);
+    }
+
+
+    public void testNine(){
+        App myApp = new App();
+        assertTrue(myApp.return_nine() == 9);
+    }
+
+
+    public void testTen(){
+        App myApp = new App();
+        assertTrue(myApp.return_ten() == 10);
+    }
+
     /**
      * Rigourous Test :-)
      */
